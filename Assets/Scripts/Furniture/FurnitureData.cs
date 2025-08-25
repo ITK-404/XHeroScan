d6a3b6f1b04d6ae8b411aeb0d07235c2c42cb18a
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-08-25 09:31:17 +0700

    Add new data structure

--- Assets/Scripts/Furniture/FurnitureData.cs
+++ Assets/Scripts/Furniture/FurnitureData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
@@ -12,4 +13,45 @@ public class FurnitureData
     public float ObjectHeight = 0.5f;
     public float rotation = 0f; // rotation in degrees around Y axis
 }
+[Serializable]
+public struct DrawItemSize
+{
+    public float width;
+    public float height;
+    public float length;
 
+    public Quaternion rotation;
+    
+    public Vector2 widthMinMax;
+    public Vector2 heightMinMax;
+    public Vector2 lengthMinMax;
+
+    public float offsetY;
+    
+}
+
+public enum ItemType
+{
+    
+}
+
+
+
+[Serializable]
+public struct DrawingTemplate
+{
+    // Instanced item can use data for reset or initialize data
+    public DrawItemSize defaultSize;
+    public ItemType defaultItemType;
+    public string description;
+}
+[Serializable]
+public struct DrawingInstanced
+{
+    public string roomID;
+    public string itemTemplateID;
+    public string itemVariantID;
+    public Vector3 worldPosition;
+    public ItemType currentItemType;
+    public DrawItemSize size;
+}
