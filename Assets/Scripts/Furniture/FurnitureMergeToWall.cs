df9741ba16271da7425250b26236cb3074de3ad0
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-09-04 13:13:38 +0700

    Add new rotation logic

--- Assets/Scripts/Furniture/FurnitureMergeToWall.cs
+++ Assets/Scripts/Furniture/FurnitureMergeToWall.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [Serializable]
 public class FurnitureMergeToWall
 {
-    private float offset;
+    public float offset;
     public float ratio;
 
     private FurnitureItem furnitureItem;
@@ -69,8 +70,6 @@ public class FurnitureMergeToWall
         furnitureItem.MoveAnchorToPositionWithoutChangeShape(CheckpointType.Bottom, firstDoorPoint);
 
         ratio = GetPointRatio(wallLine.start, wallLine.end, firstDoorPoint);
-        // cách xoay này chưa được hoàn hảo
-        // RotationToWallLine();
     }
     
     public void SnapToCurrentRoom()
@@ -212,10 +211,42 @@ public class FurnitureMergeToWall
 
     private void RotationToWallLine()
     {
-        Vector3 dir = attachedWallLine.end - attachedWallLine.start;
-        dir.y = 0;
-        float angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
-        furnitureItem.SetRotation(-angle + offset);
+        //Vector3 dir = attachedWallLine.end - attachedWallLine.start;
+        //dir.y = 0;
+        //float angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+        //furnitureItem.SetRotation(-angle + offset);
+
+        Vector3 start = attachedWallLine.start;
+        Vector3 end = attachedWallLine.end;
+
+        Vector3 dir = (end - start).normalized;
+        Vector3 left = new Vector3(-dir.z, 0, dir.x);
+        Vector3 right = -left;
+
+        Vector3 midpoint = (start + end) * 0.5f;
+        Vector3 toCenter = (GetRoomCenter() - midpoint).normalized;
+
+        // chọn pháp tuyến nào cùng hướng với vector đi vào phòng
+        Vector3 inward = (Vector3.Dot(toCenter, left) > Vector3.Dot(toCenter, right)) ? left : right;
+        Vector3 outward = -inward;
+
+        float angle = Mathf.Atan2(outward.z, outward.x) * Mathf.Rad2Deg;
+        furnitureItem.SetRotation(angle);
+    }
+
+    private Vector3 GetRoomCenter()
+    {
+        Bounds bounds = new();
+        foreach (var item in attachedRoom.wallLines)
+        {
+            if(item.type == LineType.Wall)
+            {
+                bounds.Encapsulate(item.start);
+                bounds.Encapsulate(item.end);
+            }
+        }
+
+        return bounds.center;
     }
 
     private bool IsWithinDistance(Vector3 point1, Vector3 point2, float distance)
