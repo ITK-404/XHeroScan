a4a16d8f4626ae4c28940080fae6677f75eb124c
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-09-08 13:42:51 +0700

    Add smooth camera zoom

--- Assets/Scripts/Draw2D/CameraResizeByFloor.cs
+++ Assets/Scripts/Draw2D/CameraResizeByFloor.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine;
 public class CameraResizeByFloor : MonoBehaviour
 {
     private Camera mainCamera;
-
+    private Tween moveTween;
+    private Tween sizeTween;
     public void Resize(Vector2 center, List<Vector2> checkPoints)
     {
+        moveTween?.Kill();
+        sizeTween?.Kill();
         Debug.Log("Resize camera here");
 
         Vector3 newCenter = new Vector3(center.x, mainCamera.transform.position.y, center.y);
@@ -18,8 +22,12 @@ public class CameraResizeByFloor : MonoBehaviour
         }
         float size = Mathf.Max(bounds.size.x, bounds.size.y);
 
-        mainCamera.orthographicSize = size + size * 0.1f;
-        mainCamera.transform.position = newCenter;
+        //mainCamera.transform.position = newCenter;
+        moveTween = DOVirtual.Float(mainCamera.orthographicSize, size + size * 0.1f,0.4f, (x) =>
+        {
+            mainCamera.orthographicSize = x;
+        });
+        sizeTween = mainCamera.transform.DOMove(newCenter,0.4f);
     }
 
     private void Awake()
