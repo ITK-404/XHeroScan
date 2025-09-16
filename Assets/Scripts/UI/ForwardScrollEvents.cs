79362cfc9af451455a2813e9fa2632fa95cc5d20
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-09-16 13:30:40 +0700

    Fix Cannot scroll when add input field into scroll rect

---
+++ Assets/Scripts/UI/ForwardScrollEvents.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ForwardScrollEvents : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] ScrollRect _ScrollRect;
+    private void Awake()
+    {
+        _ScrollRect = GetComponentInParent<ScrollRect>();
+    }
+    public void OnBeginDrag(PointerEventData eventData) => _ScrollRect.OnBeginDrag(eventData);
+    public void OnDrag(PointerEventData eventData) => _ScrollRect.OnDrag(eventData);
+    public void OnEndDrag(PointerEventData eventData) => _ScrollRect.OnEndDrag(eventData);
+}
--- Assets/Scripts/UI/SmartTMPInput.cs
+++ Assets/Scripts/UI/SmartTMPInput.cs
@@ -1,42 +1,22 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(CanvasGroup))]
-public class SmartTMPInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class SmartTMPInput : MonoBehaviour
 {
-    private CanvasGroup cg;
-    private TMP_InputField input;
-    private bool isDragging = false;
-    private Vector2 downPos;
-
-    void Awake()
+    private Button btn;
+    private TMP_InputField inputField;
+    private void Awake()
     {
-        cg = GetComponent<CanvasGroup>();
-        input = GetComponent<TMP_InputField>();
-        cg.blocksRaycasts = false; // mặc định cho raycast đi qua
+        btn = GetComponent<Button>();
+        inputField = GetComponentInChildren<TMP_InputField>();
+
+        //btn.onClick.AddListener(OnClickInputField);
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    private void OnClickInputField()
     {
-        downPos = eventData.position;
-        isDragging = false;
+        inputField.ActivateInputField();
     }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (Vector2.Distance(eventData.position, downPos) > 10f)
-            isDragging = true;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (!isDragging)
-        {
-            // Chỉ tap: bật lại raycast + focus input
-            cg.blocksRaycasts = true;
-            input.ActivateInputField();
-        }
-    }
-
 }
